836994df497a3d083f4da28b1f8dff0999637f87
DndStark <migangmedina@gmail.com>
2022-07-12 12:19:55 -0500

    commit miguel - deploy

---
+++ notify/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+import json
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+cred = credentials.Certificate("static/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+def index(request):
+    resgistration  = ['drdl4JErS7uIJkyVEN0Lmh:APA91bHnIDGlzi1WSW4lQzJB_iNo0wIbzNkOsE8QbrBuCsrzxat7OsdRe29tNTj5sQdlb0kywMQv-K-AZUD5hUgin3zBfUU2IRlYbYL-Ut05HgDHMYroz3_ua_-V3tH2XV6b_rhMesK6']
+    message = send_notification(resgistration , 'Test tittle' , 'Hola, solo estamos haciendo pruebas de notificación',)
+    return render(request, 'index.html', {'message' : message})
+
+def send_notification(registration_tokens , message_title , message_body, dataObject = None):
+    message = messaging.MulticastMessage(
+        notification = messaging.Notification(
+            title = message_title,
+            body = message_body
+        ),
+        data = dataObject,
+        tokens = registration_tokens        
+    )
+    
+    response = messaging.send_multicast(message)
+
+    return "Successfully sent message " + str(response)
